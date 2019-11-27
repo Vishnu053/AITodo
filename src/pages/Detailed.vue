fda209a88460a5c41a552a7766fb6d9decdeac94
vmk1315 <mewbow1315@gmail.com>
2019-11-27 22:22:27 +0530

    changes

--- src/pages/Detailed.vue
+++ src/pages/Detailed.vue
@@ -4,7 +4,7 @@
     <q-input small borderless placeholder="Enter Title..." v-model="card.title"></q-input>
     </div>
     <div v-if="card.content && showEditor">
-       <q-editor v-model="card.content" :min-height="whgt" style="background:#fff0;" class="custom-editor"/>
+       <q-editor :key="refresheditor" v-model="card.content" :min-height="whgt" style="background:#fff0;" class="custom-editor"/>
        <div style="width:100%;text-align:right;" class="q-pa-sm">
          <q-btn round outline color="primary" @click="updateDt"><q-icon name="check"></q-icon></q-btn>
          </div>
@@ -18,18 +18,34 @@ export default {
     return {
       whgt: '8rem',
       showEditor: false,
-      card: {}
+      card: {},
+      refresheditor: 0
     }
   },
   mounted () {
-    this.card = JSON.parse(localStorage.getItem('tmpnt'))
-    // localStorage.removeItem('tmpnt')
     this.whgt = (window.innerHeight - 168).toString() + 'px'
-    this.showEditor = true
+    this.getNote()
   },
   methods: {
+    getNote () {
+      this.card = {}
+      this.card = JSON.parse(localStorage.getItem('tmpnt'))
+      this.showEditor = true
+      // this.refresheditor++
+    },
     updateDt () {
       console.log(this.card)
+      var nt = JSON.parse(localStorage.getItem('AITDnotes'))
+      for (var i in nt) {
+        if (nt[i].id === this.card.id) {
+          nt.splice(i, 1)
+        }
+      }
+      nt.push(this.card)
+      localStorage.removeItem('AITDnotes')
+      setTimeout(() => {
+        localStorage.setItem('AITDnotes', JSON.stringify(nt))  
+      }, 300)
     }
   }
 }
--- src/pages/Todo.vue
+++ src/pages/Todo.vue
@@ -5,6 +5,9 @@
         <div class="text-h4 q-pb-lg q-pt-sm">Hi {{user.name}}!</div>
         <div class="text-subtitle">{{dt}}</div>
       </div>
+       <div style="width:100%;text-align:center;" class="q-pa-sm">
+         <q-btn round outline color="white" @click="newNote"><q-icon name="add"></q-icon></q-btn>
+         </div>
       <q-img src="https://source.unsplash.com/random" class="header-image absolute-top headr-blr"></q-img>
     </q-header>
     <div :style="'overflow: hidden auto; max-height:'+whgt+'px;'">
@@ -18,7 +21,7 @@
       >
         <q-card-section>
           <div class="text-h6 q-pb-md q-pt-sm">{{card.title}}</div>
-          <div class="text-subtitle2">by {{card.author}}</div>
+          <!-- <div class="text-subtitle2">by {{card.author}}</div> -->
         </q-card-section>
       </q-card>
       </div>
@@ -38,36 +41,29 @@ export default {
         name: 'Vishnu'
       },
       dt: '',
-      cards: [
-        {
-          title: 'As I Dream.',
-          author: 'Mohan',
-          content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut porro ab reprehenderit repellendus aliquam delectus hic molestiae odit, iste voluptatem id, cumque omnis rem veritatis tenetur animi vel consequuntur perferendis!'
-        },
-        {
-          title: 'These birds..',
-          author: 'Vishnu',
-          content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut porro ab reprehenderit repellendus aliquam delectus hic molestiae odit, iste voluptatem id, cumque omnis rem veritatis tenetur animi vel consequuntur perferendis!'
-        },
-        {
-          title: 'A Journey to the hills.',
-          author: 'Kala',
-          content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut porro ab reprehenderit repellendus aliquam delectus hic molestiae odit, iste voluptatem id, cumque omnis rem veritatis tenetur animi vel consequuntur perferendis!'
-        }
-      ]
+      cards: []
     }
   },
   mounted () {
     let timeStamp = Date.now()
     this.dt = date.formatDate(timeStamp, 'dddd, MMMM DD')
+    this.getNotes()
+    // var a = [{ id: 'tdx-nt-1', title: 'As I Dream', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut.' }, { id: 'tdx-nt-1', title: 'As I Dream 2', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut.' }]
+    // localStorage.setItem('AITDnotes', JSON.stringify(a))
   },
   methods: {
     getNotes () {
-      
+      this.cards = JSON.parse(localStorage.getItem('AITDnotes'))
+      console.log(this.cards)
+    },
+    newNote () {
+      var an = JSON.parse(localStorage.getItem('AITDnotes'))
+      var anc = 'tdx-nt-' + parseInt(an.length + 1)
+      var tn = { id: anc, title: 'My Title', content: 'Lorem ipsum...' }
+      this.openNote(tn)
     },
     openNote (note) {
       localStorage.setItem('tmpnt', JSON.stringify(note))
-      localStorage.setItem(note.title, JSON.stringify(note))
       this.$router.push('/note')
     }
   }
